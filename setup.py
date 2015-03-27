c7b2587fb58ef3ef56b8a41342df4184837e4242
micahhausler <micah.hausler@ambition.com>
2015-03-27 13:54:01 -0400

    Removed incorrect classifier

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
