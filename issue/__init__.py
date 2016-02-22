261d776aff748a2051a767aa9494ed852acbf370
Wes Okes <wes.okes@gmail.com>
2016-02-22 11:46:50 -0500

    fix imports

--- issue/__init__.py
+++ issue/__init__.py
@@ -1,5 +1,4 @@
 # flake8: noqa
 from .version import __version__
-from .models import Assertion, Issue, IssueManager, IssueStatus, ModelAssertion, ModelIssue, ModelIssueManager, Responder, ResponderAction
 
 default_app_config = 'issue.apps.IssueConfig'
