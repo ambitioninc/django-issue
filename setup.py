f7003c92c6f6d782e1cf8882ccc4e626cf7ac0eb
Travis Truett <travis.truett@ambition.com>
2020-03-12 17:41:45 -0400

    Pin Django

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     ],
     license='MIT',
     install_requires=[
-        'Django>=2.0',
+        'Django>=2.0,<3.0',
         'django-manager-utils>=1.4.0',
         'django-regex-field>=1.3.1',
         'jsonfield>=0.9.20',
